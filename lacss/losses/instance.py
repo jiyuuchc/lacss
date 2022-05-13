f09d7657a52c079bb00f9ef7c840eed37c5a516f
Ji Yu <jyu@uchc.edu>
2022-05-13 13:38:21 -0400

    Add more loss functions for instance_loss

--- lacss/losses/instance.py
+++ lacss/losses/instance.py
@@ -1,64 +1,101 @@
 import tensorflow as tf
 
-def self_supervised_segmentation_losses(y_pred, mask, coords, lam=1.0, beta=-2.0):
+def self_supervised_segmentation_losses(y_pred, coords, binary_mask, lam=1.0, beta=-2.):
     '''
     Args:
         y: [n_patches, patch_size, patch_size, 1]: float32, prediction 0..1
-        mask: [img_height, img_width]: int32
         coords: [n_patches, patch_size, patch_size, 2]: int32 meshgrid coordinates
+        binary_mask: [img_height, img_width]: int32
     '''
     if len(y_pred.shape) == 4:
         y_pred = tf.squeeze(y_pred, -1)
+    if len(binary_mask.shape) == 3:
+        binary_mask = tf.squeeze(binary_mask, -1)
+
     patch_size = tf.shape(y_pred)[1]
-    pad_size = patch_size//2 + 1
-    coords = coords + pad_size
+
+    padding_size = patch_size//2 + 2
+    coords = coords + padding_size
     paddings = [
-        [pad_size, pad_size],
-        [pad_size, pad_size],
+        [padding_size, padding_size],
+        [padding_size, padding_size],
     ]
-    mask = tf.pad(mask, paddings)
+    mask = tf.pad(tf.cast(binary_mask, tf.float32), paddings)
 
     log_yi = tf.math.log(tf.clip_by_value(1.0 - y_pred, tf.keras.backend.epsilon(), 1.0))
     log_yi_sum = tf.scatter_nd(coords, log_yi, tf.shape(mask))
-    log_yi_sum += (1.0 - tf.cast(mask, tf.float32)) * beta
+    log_yi_sum += (1.0 - mask) * beta
 
-    log_yi -= tf.gather_nd(log_yi_sum, coords)
+    log_yi = tf.gather_nd(log_yi_sum, coords) - log_yi
 
-    loss = - tf.reduce_mean(y_pred) + tf.reduce_mean(y_pred * log_yi) * lam
+    loss = 1.0 - tf.reduce_mean(y_pred) - tf.reduce_mean(y_pred * log_yi) * lam
 
     return loss
 
-# def cutter_loss_low_mem(y, coords, area_shape, mask = None, lam = 1.0):
-#     '''
-#     loss function that uses sparse tensor to save memory
-#     '''
-#     #area_shape = np.broadcast_to(area_shape, (coords.shape[-1],)) # in case area_shape is a number
-#     patch_shape = tuple(y.shape[1:])
-#
-#     ind0 = tf.constant(list(np.ndindex(patch_shape)), dtype = tf.dtypes.int64)
-#
-#     y_pred = tf.sigmoid(y)
-#     log_yi = tf.math.log(tf.clip_by_value(1.0 - y_pred, tf.keras.backend.epsilon(), 1.0))
-#
-#     def pad_img(cropped, offsets):
-#       ind = ind0 + tf.constant(offsets, dtype = tf.dtypes.int64)
-#       sp = tf.sparse.SparseTensor(ind, tf.reshape(cropped, [-1]), tf.constant(area_shape + patch_shape, dtype = tf.dtypes.int64))
-#       return tf.sparse.expand_dims(sp, 0)
-#
-#     log_yi_padded = tf.sparse.concat(
-#       0,
-#       [pad_img(cropped, coord) for cropped,coord in zip(tf.unstack(log_yi), list(coords))]
-#     )
-#
-#     log_yi_sum = tf.sparse.reduce_sum(log_yi_padded, axis = 0)
-#     if mask is not None:
-#       log_yi_sum += mask
-#
-#     ind = ind0 + tf.constant(coords, dtype = tf.dtypes.int64)[:, tf.newaxis, :]
-#     ind = tf.reshape(ind, tuple(log_yi.shape) + (len(patch_shape),))
-#     log_yi -= tf.gather_nd(log_yi_sum, ind)
-#
-#     loss = - tf.reduce_sum(y_pred)
-#     loss += lam * tf.reduce_sum(y_pred * log_yi) * lam
-#
-#     return loss /  y.shape[0]
+def self_supervised_segmentation_losses_2(y_pred, coords, binary_mask, lam=1.0, beta=-2.):
+    '''
+    Args:
+        y: [n_patches, patch_size, patch_size, 1]: float32, prediction 0..1
+        coords: [n_patches, patch_size, patch_size, 2]: int32 meshgrid coordinates
+        binary_mask: [img_height, img_width]: int32
+    '''
+    if len(y_pred.shape) == 4:
+        y_pred = tf.squeeze(y_pred, -1)
+    if len(binary_mask.shape) == 3:
+        binary_mask = tf.squeeze(binary_mask, -1)
+
+    patch_size = tf.shape(y_pred)[1]
+
+    padding_size = patch_size//2 + 2
+    coords = coords + padding_size
+    paddings = [
+        [padding_size, padding_size],
+        [padding_size, padding_size],
+    ]
+    mask = tf.pad(tf.cast(binary_mask, tf.float32), paddings)
+
+    log_yi = tf.math.log(tf.clip_by_value(1.0 - y_pred, tf.keras.backend.epsilon(), 1.0))
+    log_yi_sum = tf.scatter_nd(coords, log_yi, tf.shape(mask))
+
+    binary_loss = -(1.0 - mask) * log_yi_sum - mask * tf.math.log(1.0 + tf.keras.backend.epsilon() - tf.math.exp(log_yi_sum))
+    binary_loss = tf.reduce_mean(binary_loss)
+
+    log_yi = tf.gather_nd(log_yi_sum, coords) - log_yi
+
+    overlap_loss =  - tf.reduce_mean(y_pred * log_yi)
+
+    loss = binary_loss + lam * overlap_loss
+
+    return loss
+
+def supervised_segmentation_losses(y_pred, coordinates, mask_indices):
+    '''
+    Args:
+        y: [n_patches, patch_size, patch_size, 1]: float32, prediction 0..1
+        coordinates: [n_patches, patch_size, patch_size, 2] int32 meshgrid coordinates
+        mask_indices: [n_patches, None, 2] int64 Ragged tensor
+    return:
+        loss: based on cross entropy
+    '''
+    patch_shape = tf.shape(y_pred, out_type=tf.int64)
+    roi_size = patch_shape[1]
+    #top left
+    c0 = coordinates[:,0,0,:]
+    c0 = tf.repeat(c0, mask_indices.row_lengths(), axis=0)
+
+    mi_flatten = mask_indices.values - tf.cast(c0, tf.int64)
+    mi_flatten = tf.concat([mask_indices.value_rowids()[:, None], mi_flatten], axis=-1)
+    mask = tf.logical_and(
+        tf.logical_and(mi_flatten[:,1] >= 0, mi_flatten[:,1] < roi_size),
+        tf.logical_and(mi_flatten[:,2] >= 0, mi_flatten[:,2] < roi_size),
+        )
+    mi_flatten = tf.boolean_mask(mi_flatten, mask)
+
+    n_pixels = tf.shape(mi_flatten)[0]
+    gt_patches = tf.scatter_nd(mi_flatten, tf.ones([n_pixels], tf.int32), shape=patch_shape[:3])
+    gt_patches = tf.expand_dims(gt_patches, -1)
+
+    loss = tf.keras.losses.binary_crossentropy(gt_patches, y_pred)
+    loss = tf.reduce_mean(loss) * tf.cast(patch_shape[0], loss.dtype)
+
+    return loss
