24a15906cf31e197c7b9ec8ed108ae8cb3e81ff2
Ji Yu <jyu@uchc.edu>
2022-05-13 13:33:01 -0400

    remove hardcoded roi size in detection_head

--- lacss/losses/detection.py
+++ lacss/losses/detection.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 
-def locations_to_labels(locations, target_shape, threshold):
+def locations_to_labels(locations, target_shape, threshold=1.5):
     ''' Generate labels as score regression targets
     Args:
         locations: [batch_size, N, 2] float32 true location values. scaled 0..1
@@ -30,7 +30,7 @@ def locations_to_labels(locations, target_shape, threshold):
     best_distances = tf.experimental.numpy.take_along_axis(distances_sq, indices[:,None,:,:], 1)
     best_distances = tf.squeeze(best_distances, 1)
 
-    score_target = tf.where(best_distances < 1.5, 1, 0)
+    score_target = tf.cast(tf.where(best_distances < threshold, 1, 0), tf.float32)
     score_target = tf.expand_dims(score_target, -1)
 
     indices = tf.repeat(indices[:, None, :, :, None], 2, axis=-1)
@@ -39,13 +39,13 @@ def locations_to_labels(locations, target_shape, threshold):
 
     return score_target, regression_target
 
-def detection_losses(gt_locations, pred_scores, pred_regressions, threshold=1.5, **kwargs):
+def detection_losses(gt_locations, pred_scores, pred_regressions, roi_size=1.5, **kwargs):
     '''
     Args:
         gt_locations: [batch_size, N, 2] float32 true location values. scaled 0..1
         pred_scores: [batch_size, h, w, 1] detector score output
         pred_regressions: [batch_size, h, w, 2] detector regression output
-        threshold: float number, the size of detection ROI
+        roi_size: float number, the size of detection ROI
     Returns:
         score_loss: float
         regression_loss: float
@@ -55,7 +55,7 @@ def detection_losses(gt_locations, pred_scores, pred_regressions, threshold=1.5,
     target_shape = tf.shape(pred_scores)[1:3]
     batch_size = tf.shape(pred_scores)[0]
 
-    score_target, regression_target = locations_to_labels(gt_locations, target_shape, threshold)
+    score_target, regression_target = locations_to_labels(gt_locations, target_shape, roi_size)
 
     score_loss = tf.keras.losses.binary_focal_crossentropy(score_target, pred_scores, **kwargs)
     score_loss = tf.reduce_sum(tf.reduce_mean(tf.reshape(score_loss, [batch_size, -1]), axis=1))
@@ -63,7 +63,7 @@ def detection_losses(gt_locations, pred_scores, pred_regressions, threshold=1.5,
     regression_loss = tf.keras.losses.huber(regression_target, pred_regressions)
     regression_loss = tf.reshape(regression_loss, [batch_size, -1])
     score_target = tf.reshape(score_target, [batch_size, -1])
-    regression_loss = tf.reduce_sum(tf.where(score_target>0, regression_loss, 0), axis=1) / (tf.cast(tf.reduce_sum(score_target, axis=1), tf.float32) + 1e-9)
+    regression_loss = tf.reduce_sum(tf.where(score_target>0, regression_loss, 0), axis=1) / (tf.reduce_sum(score_target, axis=1)+ 1e-9)
     regression_loss = tf.reduce_sum(regression_loss)
 
     return score_loss, regression_loss
