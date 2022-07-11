4787ef397023fccb4cc3517b9ac93ceb662fbfe5
Ji Yu <jyu@uchc.edu>
2022-07-11 10:41:27 -0400

    moving most nn files into a new layers directory

---
+++ lacss/layers/__init__.py
@@ -0,0 +1,6 @@
+from .unet import UNet
+from .resnet import ResNet
+from .lpn import LPN
+from .segmentor import Segmentor
+from .detector import Detector
+from .xcit import XCiT
--- lacss/models/__init__.py
+++ lacss/models/__init__.py
@@ -1,8 +1,1 @@
-# from .shuffle_net import *
-from .unet import UNet
-from .resnet import ResNet
-from .lpn import LPN
-from .segmentor import Segmentor
 from .lacss import LacssModel
-from .detector import Detector
-from .xcit import XCiT
