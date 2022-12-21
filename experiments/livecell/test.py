3a68a28251ea88e7f5f0f2b1ea1bb8f08b518774
Ji Yu <jyu@uchc.edu>
2022-12-21 10:51:05 -0500

    add AJI metric

--- experiments/livecell/test.py
+++ experiments/livecell/test.py
@@ -11,6 +11,9 @@ import lacss
 import data
 import tqdm
 
+from lacss.metrics import AJI
+min_score = 0.4
+
 def format_array(arr):
     s = [f'{v:.4f}' for v in arr]
     s = ',\t'.join(s)
@@ -32,13 +35,14 @@ def run_test(data_path, log_dir, checkpoint='model_weights'):
     with open(join(log_dir, 'config.json')) as f:
         model_cfg = json.load(f)
     model_cfg['test_max_output']=3000
-    model_cfg['test_min_score']=0.4
+    # model_cfg['test_min_score']=min_score
     model = lacss.models.LacssModel.from_config(model_cfg)
     model.load_weights(join(log_dir, checkpoint)).expect_partial()
     print(f'model loaded from {checkpoint}')
 
     thresholds = [.5, .55, .6, .65, .7, .75, .8, .85, .9, .95]
     cell_APs = {}
+    aji = {}
     mAP = lacss.metrics.MaskMeanAP(thresholds)
     for x in tqdm.tqdm(ds_test):
         xx = lacss.data.parse_test_data_func(x)
@@ -57,8 +61,15 @@ def run_test(data_path, log_dir, checkpoint='model_weights'):
         c = x['cell_type'].numpy()
         if not c in cell_APs:
             cell_APs[c] = lacss.metrics.MaskMeanAP(thresholds)
+            aji[c] = AJI()
         cell_APs[c].update_state(gt, pred, scores)
         mAP.update_state(gt, pred, scores)
+
+        gt_mi = tf.gather(gt_mi, scores > min_score)
+        gt_bboxes = tf.gather(gt_bboxes, scores > min_score)
+        patches = tf.gather(patches, scores > min_score)
+        coords = tf.gather(coords, scores > min_score)
+        aji[c].update(gt_mi, gt_bboxes, patches, coords)
 
     cell_types = list(cell_APs.keys())
     cell_types.sort()
@@ -79,6 +90,11 @@ def run_test(data_path, log_dir, checkpoint='model_weights'):
         print(f'{c}: {fnr_str}')
     fnr_str = format_array(fnrs(mAP))
     print(f'all: {fnr_str}')
+
+    print('AJIs...'')
+    for c in cell_types:
+        result = aji[c].result()
+        print(f'{c}: {result}')
 
 if __name__ =="__main__":
     parser = argparse.ArgumentParser(description='Test livecell model')
--- lacss/ops/mask_iou_similarity.py
+++ lacss/ops/mask_iou_similarity.py
@@ -1,5 +1,53 @@
 import tensorflow as tf
 from .iou_similarity import *
+from ..ops import *
+
+def has_box_intersection(gt_box, pred_box):
+    gt_box = tf.cast(gt_box, tf.float32)
+    pred_box = tf.cast(pred_box, tf.float32)
+
+    y_min1, x_min1, y_max1, x_max1 = tf.split(gt_box, 4, -1)
+    y_min2, x_min2, y_max2, x_max2 = tf.split(pred_box, 4, -1)
+
+    y_min_max = tf.minimum(y_max1, tf.transpose(y_max2))
+    y_max_min = tf.maximum(y_min1, tf.transpose(y_min2))
+    x_min_max = tf.minimum(x_max1, tf.transpose(x_max2))
+    x_max_min = tf.maximum(x_min1, tf.transpose(x_min2))
+
+    intersect_heights = y_min_max - y_max_min
+    intersect_widths = x_min_max - x_max_min
+
+    return (intersect_heights > 0) & (intersect_widths > 0)
+
+def mask_intersects(gt_mi, gt_box, pred_patches, pred_patch_co, th=0.5):
+    pred_box = bboxes_of_patches(pred_patches, pred_patch_co, th)
+    ids = tf.where(has_box_intersection(gt_box, pred_box))
+    gt_ids = ids[:, 0]
+    pred_ids = ids[:, 1]
+
+    pred_patches = tf.gather(pred_patches>th, pred_ids)
+    pred_patches = tf.squeeze(pred_patches, -1)
+    y0x0 = tf.gather(pred_patch_co[:, 0, 0, :], pred_ids)
+    gt_mi_x = tf.gather(gt_mi, gt_ids)
+    y0x0 = tf.repeat(y0x0, gt_mi_x.row_lengths(), axis=0)
+    fns = gt_mi_x.value_rowids()[..., None]
+    values = gt_mi_x.values - tf.cast(y0x0, gt_mi_x.dtype)
+    gt_mi_flat = tf.concat([fns, values], axis=-1)
+    gt_patches = tf.scatter_nd(gt_mi_flat, tf.ones(values.shape[0], dtype=tf.float32), pred_patches.shape)
+    gt_patches = tf.cast(gt_patches, tf.bool)
+
+    its = tf.math.count_nonzero(gt_patches & pred_patches, axis=(1,2))
+    intersects = tf.scatter_nd(ids, its, shape=[gt_box.shape[0], pred_box.shape[0]])
+    return intersects
+
+
+def mask_ious(gt_mi, gt_box, pred_patches, pred_patch_co, th=0.5):
+    gt_areas = gt_mi.row_lengths()
+    pred_areas = tf.count_nonzero(pred_patches > th, axis=(1,2,3))
+    sum_areas = gt_areas[:, None] + pred_areas
+    intersects = mask_intersects(gt_mi, gt_box, pred_patches, pred_patch_co, th)
+    ious = tf.cast(intersects, tf.float32) / (tf.cast(sum_areas - intersects, tf.float32) + 1e-8)
+    return ious
 
 def mask_iou_similarity(gt, pred, patch_size=96):
     ''' Compute mask_iou matrix.
