218f1bb6402528507c264e066f1812279e15a57a
Ji Yu <jyu@uchc.edu>
2024-02-27 17:40:05 -0500

    Further implement remote server.
      - default to accept local connection.
      - add a token auth scheme

--- lacss/deploy/remote_server.py
+++ lacss/deploy/remote_server.py
@@ -14,6 +14,8 @@ import typer
 from .predict import Predictor
 from .proto import lacss_pb2, lacss_pb2_grpc
 
+_AUTH_HEADER_KEY = "authorization"
+
 app = typer.Typer(pretty_exceptions_enable=False)
 
 def process_input(msg):
@@ -37,6 +39,25 @@ def process_result(polygons, scores):
             msg.polygons.append(polygon_msg)
 
     return msg    
+
+class TokenValidationInterceptor(grpc.ServerInterceptor):
+    def __init__(self, token):
+        def abort(ignored_request, context):
+            context.abort(grpc.StatusCode.UNAUTHENTICATED, "Invalid signature")
+
+        self._abort_handler = grpc.unary_unary_rpc_method_handler(abort)
+        self.token = token
+
+    def intercept_service(self, continuation, handler_call_details):
+        # Example HandlerCallDetails object:
+        #     _HandlerCallDetails(
+        #       method=...,
+        #       invocation_metadata=...)
+        expected_metadata = (_AUTH_HEADER_KEY, f"Bearer {self.token}")
+        if expected_metadata in handler_call_details.invocation_metadata:
+            return continuation(handler_call_details)
+        else:
+            return self._abort_handler
 
 class LacssServicer(lacss_pb2_grpc.LacssServicer):
     def __init__(self, model):
@@ -74,28 +95,59 @@ class LacssServicer(lacss_pb2_grpc.LacssServicer):
 
         return result
 
+def get_predictor(modelpath):
+    model = Predictor(modelpath)
+
+    logging.info(f"lacss_server: loaded model from {modelpath}")
+
+    model.detector.test_max_output = 512 # FIXME good default?
+    model.detector.test_min_score = 0.4
+
+    return model
+
 @app.command()
 def main(
     modelpath: Path,
     port: int = 50051,
     workers: int = 10,
+    local: bool = False,
+    token: bool = False,
     debug: bool = False,
 ):
     logging.basicConfig(level=logging.DEBUG if debug else logging.INFO)
-    # logging.basicConfig(level=logging.INFO)
 
-    model = Predictor(modelpath)
-    model.detector.test_max_output = 512
-    model.detector.test_min_score = 0
+    model = get_predictor(modelpath)
 
-    logging.info(f"lacss_server: loaded model from {modelpath}")
     logging.info(f"lacss_server: default backend is {jax.default_backend()}")
+
     if (jax.default_backend() == "cpu"):
         logging.warn(f"lacss_server: WARNING: No GPU configuration. This might be very slow ...")
 
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=workers))
+    if token:
+        import secrets
+
+        token = secrets.token_urlsafe()
+
+        server = grpc.server(
+            futures.ThreadPoolExecutor(max_workers=workers), 
+            interceptors=(TokenValidationInterceptor(token),),
+        )
+
+        print("===================================")
+        print(f"For access use token: {token}")
+        print("===================================")
+    else:
+        server = grpc.server(futures.ThreadPoolExecutor(max_workers=workers))
+
     lacss_pb2_grpc.add_LacssServicer_to_server(LacssServicer(model), server)
-    server.add_insecure_port(f"[::]:{port}")
+
+    if local:
+        server.add_secure_port(f"localhost:{port}", grpc.local_server_credentials())
+    else:
+        server.add_insecure_port(f"localhost:{port}")
+
+    logging.info(f"lacss_server: listening on port {port}")
+
     server.start()
     server.wait_for_termination()
 
