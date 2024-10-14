527465c719262d1b924e23f12585b5b0b7920f31
Ji Yu <jyu@uchc.edu>
2024-10-14 14:57:03 +0000

    update model urls

--- lacss/deploy/__init__.py
+++ lacss/deploy/__init__.py
@@ -2,7 +2,12 @@ from typing import Mapping
 from .predict import Predictor
 
 model_urls: Mapping[str, str] = {
-    "lacss_v2_default": "https://huggingface.co/yulabuchc/lacss_v2_default/resolve/main/LACSSV2-DEFAULT-240927"
+    "lacss3-small": "https://huggingface.co/jiyuuchc/lacss3-small/resolve/main/lacss3-small",
+    "lacss3-base": "https://huggingface.co/jiyuuchc/lacss3-base/resolve/main/lacss3-base",
+    "lacss3-small-cellpose": "https://huggingface.co/jiyuuchc/lacss3-small-cellpose/resolve/main/lacss3-small-c",
+    "lacss3-small-livecell": "https://huggingface.co/jiyuuchc/lacss3-small-livecell/resolve/main/lacss3-small-l",
+    "lacss3-small-nips": "https://huggingface.co/jiyuuchc/lacss3-small-nips/resolve/main/lacss3-small-n",
 }
-model_urls["default"] = model_urls["lacss_v2_default"]
+
+model_urls["default"] = model_urls["lacss3-base"]
 
