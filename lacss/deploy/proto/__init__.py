92b2338b02988b8b60b5b2c4841adac109c3d4f3
Ji Yu <jyu@uchc.edu>
2024-11-26 18:28:02 +0000

    set biopb as a dependency instead of subtree

--- lacss/deploy/proto/__init__.py
+++ lacss/deploy/proto/__init__.py
@@ -1,11 +1,11 @@
-import sys
-from pathlib import Path
-sys.path.append(str(Path(__file__).parent)) # hack to get around grpc_python's regid design
+from biopb.image.rpc_object_detection_pb2_grpc import ObjectDetection as Lacss
+from biopb.image.rpc_object_detection_pb2_grpc import ObjectDetectionServicer as LacssServicer
+from biopb.image.rpc_object_detection_pb2_grpc import ObjectDetectionStub as LacssStub
+from biopb.image.rpc_object_detection_pb2_grpc import add_ObjectDetectionServicer_to_server as add_LacssServicer_to_server
 
-from biopb.lacss.lacss_pb2_grpc import Lacss, LacssServicer, LacssStub, add_LacssServicer_to_server
-from biopb.lacss.detection_request_pb2 import DetectionRequest
-from biopb.lacss.detection_response_pb2 import DetectionResponse, ScoredROI
-from biopb.lacss.bindata_pb2 import BinData
-from biopb.lacss.detection_settings_pb2 import DetectionSettings
-from biopb.lacss.image_data_pb2 import ImageData, Pixels, ImageAnnotation
-from biopb.lacss.roi_pb2 import ROI, Rectangle, Mask, Mesh, Polygon, Point
+from biopb.image.detection_request_pb2 import DetectionRequest
+from biopb.image.detection_response_pb2 import DetectionResponse, ScoredROI
+from biopb.image.bindata_pb2 import BinData
+from biopb.image.detection_settings_pb2 import DetectionSettings
+from biopb.image.image_data_pb2 import ImageData, Pixels, ImageAnnotation
+from biopb.image.roi_pb2 import ROI, Rectangle, Mask, Mesh, Polygon, Point
--- proto/README.md
+++
@@ -1,8 +0,0 @@
-# biopb
-A place for collecting protobuf definitions for bio-research data. Currently it has only two packages
-
-1. `biopb.ome` Microscopy data representation modeled after [OME-XML](https://ome-model.readthedocs.io/en/stable/ome-xml/index.html).
-2. `biopb.lacss` A GRPC protocol for single-cell segmentation services, designed originally for the [Lacss](https://github.com/jiyuuchc/lacss/) project.
-
-## Documentation
-[Documentation](https://jiyuuchc.github.io/biopb/)
