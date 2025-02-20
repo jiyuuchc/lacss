a467d7cc2894ccafc0221d0e62b5d6bc0e2fa95f
Ji Yu <jyu@uchc.edu>
2025-02-20 16:20:53 +0000

    pytest cases related to grpc

---
+++ tests/deploy/conftest.py
@@ -0,0 +1,97 @@
+from pathlib import Path
+
+import pytest
+import grpc
+
+MODULE_DIR = Path(__file__).parent
+
+@pytest.fixture(scope='package')
+def model_file():
+    import urllib.request
+
+    cache_dir = Path.home() / ".cache" / "lacss"
+    model_file = cache_dir / "lacss3_small"
+
+    if not model_file.exists():
+        cache_dir.mkdir(parents=True, exist_ok=True)
+        
+        urllib.request.urlretrieve(
+            "https://huggingface.co/jiyuuchc/lacss3-small/resolve/main/lacss3-small?download=true",
+            model_file,
+        )
+
+    yield model_file
+
+
+@pytest.fixture(scope='package')
+def model(model_file):
+    from lacss.deploy.predict import Predictor
+
+    predictor = Predictor(model_file)
+
+    yield predictor
+
+    del predictor
+
+
+@pytest.fixture(scope='package')
+def model_f16(model_file):
+    from lacss.deploy.predict import Predictor
+
+    predictor = Predictor(model_file, f16=True)
+
+    yield predictor
+
+    del predictor
+
+
+@pytest.fixture(scope='package')
+def test_image_2d():
+    import tifffile
+    img = tifffile.imread(MODULE_DIR / "test_data" / "test_2d.tif")
+
+    yield img
+
+    del img
+
+
+@pytest.fixture(scope='package')
+def test_image_3d():
+    import tifffile
+    img = tifffile.imread(MODULE_DIR / "test_data" / "test_3d.tif")
+
+    yield img
+
+    del img
+
+
+@pytest.fixture(scope='package')
+def grpc_server(model_f16):
+    from concurrent import futures
+    import lacss.deploy.proto as proto
+
+    from lacss.deploy.remote_server import LacssServicer
+
+    _MAX_MSG_SIZE = 1024 * 1024 * 16
+    endpoint = "127.0.0.1:50051"
+
+    server = grpc.server(
+        futures.ThreadPoolExecutor(max_workers=4),
+        options=(("grpc.max_receive_message_length", _MAX_MSG_SIZE),),
+    )
+    proto.add_LacssServicer_to_server(
+        LacssServicer(model_f16), server
+    )
+    server.add_insecure_port(endpoint)
+
+    server.start()
+
+    yield endpoint
+
+    server.stop(grace=None)
+
+
+@pytest.fixture(scope='package')
+def grpc_channel(grpc_server):
+    with grpc.insecure_channel(grpc_server) as channel:
+        yield channel
---
+++ tests/deploy/grpc_test.py
@@ -0,0 +1,57 @@
+import pytest
+import grpc
+
+import biopb.image as proto
+from biopb.image.utils import serialize_from_numpy
+
+_MAX_MSG_SIZE = 1024 * 1024 * 16
+
+def test_grpc_2d(grpc_channel, test_image_2d):
+    pixels = serialize_from_numpy(test_image_2d)
+
+    stub = proto.ObjectDetectionStub(grpc_channel)
+
+    request = proto.DetectionRequest(
+        image_data=proto.ImageData(pixels=pixels),
+    )
+
+    response = stub.RunDetection(request, timeout=60)
+
+    assert len(response.detections) == 22
+
+    request = proto.DetectionRequest(
+        image_data=proto.ImageData(pixels=pixels),
+        detection_settings=proto.DetectionSettings(
+            nms_iou=0.4,
+            scaling_hint=1.0,
+        )
+
+    )
+
+    response = stub.RunDetection(request)
+
+    assert len(response.detections) == 22
+
+
+def test_grpc_3d(grpc_channel, test_image_3d):
+    pixels = serialize_from_numpy(
+        test_image_3d[..., None],
+        physical_size_x=1.0,
+        physical_size_y=1.0,
+        physical_size_z=4.0,
+    )
+
+    stub = proto.ObjectDetectionStub(grpc_channel)
+
+    request = proto.DetectionRequest(
+        image_data=proto.ImageData(pixels=pixels),
+        detection_settings=proto.DetectionSettings(
+            min_score=0.3,
+            nms_iou=0.4,
+            scaling_hint=1.0,
+        )
+    )
+
+    response = stub.RunDetection(request, timeout=120)
+
+    assert len(response.detections) > 0
